b9adc18e98f8500097fc16a5b1f78a4b575a2016
jon-cundiff <jon.cundiff@gmail.com>
2021-11-03 22:19:12 -0500

    footer mobile styling. js footer placement behavior start

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-const menuObj = document.querySelector('#menu-button');
+const menuObj = document.querySelector('.menu-button');
 const menuHeader = document.querySelector('.menu-header');
-let menuIcon;
-menuObj.addEventListener('load', () => {
-    // Need to wait until Object element loads before can grab elements in the SVG
-    menuIcon = menuObj.contentDocument.querySelector('#menu-icon');
-    menuIcon.addEventListener('click', menuClick);
-});
 
 let menuActive = false;
 
@@ -15,3 +9,33 @@ const menuClick = e => {
     menuIcon.classList[clMethod]('menu-active');
     menuHeader.classList[clMethod]('menu-active');
 }
+
+const adjustFooterToContentSize = (e) => {
+    console.log(e)
+    if (e.type === 'resize' && window.innerWidth < 400) {
+        return;
+    }
+    const sh = document.scrollingElement.scrollHeight;
+    const footer = document.querySelector('.footer');
+    if (sh > window.innerHeight) {
+        footer.style.bottom = `${window.innerHeight - sh}px`;
+    } else {
+        footer.style.bottom = '0px';
+    }
+}
+
+let menuIcon;
+menuObj.addEventListener('load', () => {
+    // Need to wait until Object element loads before can grab elements in the SVG
+    menuIcon = menuObj.contentDocument.querySelector('.menu-svg');
+    menuIcon.addEventListener('click', menuClick);
+});
+
+window.onload = window.onresize = adjustFooterToContentSize;
+
+const para = document.querySelector('p');
+
+para.addEventListener('click', () => {
+    para.style.display = 'none';
+    para.style.visibility = 'hidden';
+})
